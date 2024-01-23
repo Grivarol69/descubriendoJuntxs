26cb1df3c8d3eaa0c766b8dba8813edf67468a3b
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-23 16:45:27 -0500

    Add styles in navbar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
