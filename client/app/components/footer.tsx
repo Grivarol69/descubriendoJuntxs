b24c41e73cd4e71481317c51bbabfeb8a0a9884e
isabella <isatorrente10@gmail.com>
2024-01-24 14:54:53 -0500

    social media

---
+++ client/app/components/footer.tsx
@@ -0,0 +1,19 @@
+'use client'
+import react from 'react';
+const Footer = () => {
+    return (
+        <div>
+            <footer >
+                <p>© 2023 SoyHenry. All Rights Reserved.</p>
+                <div>
+                    <a href="https://www.facebook.com/people/Descubriendo-Juntxs/100083237715538/?mibextid=LQQJ4d" target="_blank">Facebook</a>
+                    <a href="https://www.twitter.com/yourcompany" target="_blank">Twitter</a>
+                    <a href="https://www.instagram.com/descubriendojuntxs/" target="_blank">Instagram</a>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
+
