ea8d710d8917f6602cbc225b9288ff354efa5fe8
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-25 18:07:12 -0500

    Add Links and logo in footer

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Logo from "../../public/assets/Logo.svg";
+import Logo from "../../public/assets/Logo-Nombre.svg";
 import User from "../../public/assets/User.svg";
 import Menu from "../../public/assets/Menu.svg";
 import Link from "next/link";
