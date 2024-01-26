b08801c946c92439392a10e27fbcb6cd728ae5c2
isabella <isatorrente10@gmail.com>
2024-01-26 13:50:10 -0500

    boton flecha

--- client/app/pages/user/myprofile/page.tsx
+++ client/app/pages/user/myprofile/page.tsx
@@ -1,15 +1,39 @@
 'use client'
-import RootLayout from "../layout"
+import style from './myProfile.module.css'
 
 const Profile = () => {
     return (
         <div>
-            <h1>Mi Perfil</h1>
-            <h2>Mis datos</h2>
-            <h2>Seguridad</h2>
-            <h2>Tarjetas</h2>
-            <h2>Privacidad</h2>
-            <h2>Terminos y Condiciones</h2>
+            <h1>Mi Perfil
+                <button >
+                    <span className={style.arrow}>-&gt;</span>
+                </button>
+            </h1>
+            <h2>Mis datos
+                <button >
+                    <span className={style.arrow}>-&gt;</span>
+                </button>
+            </h2>
+            <h2>Seguridad
+                <button >
+                    <span className={style.arrow}>-&gt;</span>
+                </button>
+            </h2>
+            <h2>Tarjetas
+                <button >
+                    <span className={style.arrow}>-&gt;</span>
+                </button>
+            </h2>
+            <h2>Privacidad
+                <button >
+                    <span className={style.arrow}>-&gt;</span>
+                </button>
+            </h2>
+            <h2>Terminos y Condiciones
+                <button >
+                    <span className={style.arrow}>-&gt;</span>
+                </button>
+            </h2>
         </div>
     )
 }
