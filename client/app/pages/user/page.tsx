2a0b1f2cd0ebc5e7bac6dbbe359093ed3287718e
Luisfgt <luisfgonzalezt09@gmail.com>
2024-01-27 07:39:45 -0500

    merge pages

--- client/app/pages/user/page.tsx
+++ client/app/pages/user/page.tsx
@@ -5,12 +5,36 @@ const Profile = () => {
     return (
         <div className={style.gridColumns}>
             <div className={style.cardMyProfile}>
-                <h1>Mi Perfil</h1>
-                <h2>Mis datos</h2>
-                <h2>Seguridad</h2>
-                <h2>Tarjetas</h2>
-                <h2>Privacidad</h2>
-                <h2>Terminos y Condiciones</h2>
+                    <h1>Mi Perfil
+                        <button >
+                            <span className={style.arrow}>-&gt;</span>
+                        </button>
+                    </h1>
+                    <h2>Mis datos
+                        <button >
+                            <span className={style.arrow}>-&gt;</span>
+                        </button>
+                    </h2>
+                    <h2>Seguridad
+                        <button >
+                            <span className={style.arrow}>-&gt;</span>
+                        </button>
+                    </h2>
+                    <h2>Tarjetas
+                        <button >
+                            <span className={style.arrow}>-&gt;</span>
+                        </button>
+                    </h2>
+                    <h2>Privacidad
+                        <button >
+                            <span className={style.arrow}>-&gt;</span>
+                        </button>
+                    </h2>
+                    <h2>Terminos y Condiciones
+                        <button >
+                            <span className={style.arrow}>-&gt;</span>
+                        </button>
+                    </h2>
             </div>
         </div>
     )
