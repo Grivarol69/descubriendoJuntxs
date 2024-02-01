cf5e26729e00e7360ef76cd4984ea8465b3ddeb2
DiegoHrz <hernandezwdiego@gmail.com>
2024-02-01 15:54:28 -0500

    Add donaciones-icon

--- client/app/donaciones/corporativa/page.tsx
+++ client/app/donaciones/corporativa/page.tsx
@@ -19,32 +19,39 @@ const DonacionesCorporativasPage: React.FC = () => {
   };
 
   return (
-    <div style={styles}>
-      <h1>Donaciones Corporativas</h1>
-      <label>
-        Proyecto:
-        <select>
-          <option value="proyecto1">Proyecto 1</option>
-          <option value="proyecto2">Proyecto 2</option>
-        </select>
-        <button> Ver proyecto </button>
-      </label>
+    <div className="border">
+      <div className="flex flex-col">
+        <h1 className="text-3xl">Donaciones Corporativas</h1>
+        <div className="border flex">
+        <div className="w-1/2 border">
+          <label>
+            Proyecto:
+            <select>
+              <option value="proyecto1">Proyecto 1</option>
+              <option value="proyecto2">Proyecto 2</option>
+            </select>
+            <button> Ver proyecto </button>
+          </label>
 
-      <div>
-        <h2>Beneficios fiscales:</h2>
-        <ul >
-          <li>✓ Menos impuestos sobre el empleado</li>
-          <li>✓ Deducciones fiscales...</li>
-        </ul>
-      </div>
+          <div>
+            <h2>Beneficios fiscales:</h2>
+            <ul>
+              <li>✓ Menos impuestos sobre el empleado</li>
+              <li>✓ Deducciones fiscales...</li>
+            </ul>
+          </div>
 
-      <div>
-        <h2>Descripción de la donación:</h2>
-        <p>
-          Aca tengo que agregar el texto de la descripcion de la donacion.
-          {/* Agrega más contenido según sea necesario y tambien hacer un overflow = scroll */}
-        </p>
-        <button> Donar con Paypal</button>
+          <div>
+            <h2>Descripción de la donación:</h2>
+            <p>
+              Aca tengo que agregar el texto de la descripcion de la donacion.
+              {/* Agrega más contenido según sea necesario y tambien hacer un overflow = scroll */}
+            </p>
+            <button> Donar con Paypal</button>
+          </div>
+        </div>
+        <div className="w-1/2 border">d</div>
+        </div>
       </div>
     </div>
   );
