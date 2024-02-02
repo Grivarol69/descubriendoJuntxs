a1e0c9a9ebefe7d1c12eeadb178906f92d0a99bf
isabella <isatorrente10@gmail.com>
2024-02-02 01:05:46 -0500

    nueva rama con env oculto

--- client/app/pages/signup/page.tsx
+++ client/app/pages/signup/page.tsx
@@ -3,10 +3,10 @@
 import { FormEvent, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import signIn from "@/app/firebase/auth/signIn";
-import Footer from "@/components/Footer/Footer";
 import signUp from "@/app/firebase/auth/signup";
 import signUpWithGoogle from "@/app/firebase/auth/signInWithGoogle";
+import { ValidateForm } from "@/app/firebase/validation";
+
 
 const SignUpPage = () => {
     const [infoUser, setInfoUser] = useState({
@@ -14,22 +14,68 @@ const SignUpPage = () => {
         email: '',
         password: ''
     })
+
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+
     const router = useRouter()
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInfoUser({
-            ...infoUser,
-            [e.target.name]: e.target.value
-        })
-    }
+        const { name, value } = e.target;
+
+        setInfoUser(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+
+        let error = '';
+
+        switch (name) {
+            case 'name':
+                error = !value ? 'El nombre es obligatorio' : '';
+                break;
+            case 'email':
+                if (!value) {
+                    error = 'El correo electrónico es obligatorio';
+                } else if (!/\S+@\S+\.\S+/.test(value)) {
+                    error = 'El correo electrónico no es válido';
+                }
+                break;
+            case 'password':
+                if (!value) {
+                    error = 'La contraseña es obligatoria';
+                } else if (value.length < 7 || !/[A-Za-z]/.test(value) || !/\d/.test(value)) {
+                    error = 'La contraseña debe tener al menos 7 caracteres, un numero y una letra';
+                }
+                break;
+        }
+
+        setErrors(prevState => ({
+            ...prevState,
+            [name]: error
+        }));
+    };
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        // Handle form submission here
+
         event.preventDefault()
+        // const formErrors = ValidateForm(infoUser);
+        // setErrors(formErrors);
+
+        // if (Object.values(formErrors).some(error => error !== '')) {
+        //     return;
+        // }
+
         try {
             console.log('hola');
             const { result, error } = await signUp(infoUser.email, infoUser.password)
             if (error) {
+                setErrorMessage('Error al registrarse');
                 return console.log(error);
             }
             console.log(result);
@@ -40,12 +86,12 @@ const SignUpPage = () => {
     };
 
     const handleGoogleSignUp = async (event: any) => {
-        // Handle Google sign up here with firebase
         event.preventDefault()
         try {
             console.log('hola');
             const { result, error } = await signUpWithGoogle()
             if (error) {
+                setErrorMessage('Error al registrarse con Google');
                 return console.log(error);
             }
             console.log(result);
@@ -64,24 +110,27 @@ const SignUpPage = () => {
                 <label >
                     Nombre Completo:
                     <input type="text" name="name" value={infoUser.name} placeholder="Juan Pérez" onChange={handleChange} />
+                    {errors.name && <p>{errors.name}</p>}
                 </label>
                 <label>
                     Correo Electronico:
                     <input type="text" name="email" value={infoUser.email} placeholder="ejemplo@dominio.com" onChange={handleChange} />
+                    {errors.email && <p>{errors.email}</p>}
                 </label>
                 <label>
                     Contraseña:
                     <input type="password" name="password" value={infoUser.password} placeholder="Contraseña segura" onChange={handleChange} />
+                    {errors.password && <p>{errors.password}</p>}
                 </label>
-                <button type="submit">Registrarse</button>
+                {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                <button
+                    disabled={Object.values(errors).some(error => error !== '') || !infoUser.name || !infoUser.email || !infoUser.password}
+                    type="submit">Registrarse</button>
                 <div>
                     <button onClick={handleGoogleSignUp}> Registrarse Con Google </button>
                 </div>
             </form>
-            {/* <div>
-                <Footer />
-            </div> */}
-        </div>
+        </div >
     )
 }
 
