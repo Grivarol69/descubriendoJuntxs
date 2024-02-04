ae1917c8ca6799cf337c46774939b0b7960c0e69
brigitte <brigittebedo@hotmail.com>
2024-02-04 18:47:06 -0500

    cloudinary landing

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import BlurArrow from "../public/assets/blue-button.svg";
 import Gradient from "../public/assets/Gradient.svg";
@@ -8,6 +9,7 @@ import Truspilot from "../public/assets/Trustpilot.svg";
 import Cnn from "../public/assets/CNN.svg";
 import Cluth from "../public/assets/Clutch.svg";
 import Link from "next/link";
+import { CldImage } from "next-cloudinary";
 
 const Hero =()=> {
   return (
@@ -35,20 +37,19 @@ const Hero =()=> {
         </div>
       </div>
 
-      <div className="relative flex h-full w-full justify-center">
-        <Image
-          src={Gradient}
-          alt="Gradient"
-          className="min-h-[500px] w-full object-cover lg:h-auto"
-        />
-
-        <div className="absolute bottom-5 flex w-full flex-col items-center">
-          <Image
-            src={HeroImage}
+        
+      <div className="relative flex h-full w-full justify-center" >
+     
+          <CldImage
+            src="hfysg7pp37j0vittsk7t"
+            width="1300"
+            height="1133"
+            
             alt="hero image"
             className="-ml-4 h-[310px] sm:-mb-20 sm:h-[400px] lg:-mb-28 lg:h-auto xl:w-[70%]"
           />
 
+        <div className="absolute bottom-5 flex w-full flex-col items-center">
           <div className="flex w-full flex-col items-center lg:container lg:flex-row lg:justify-between lg:px-20">
             <p className="text-[#FFFFFF] text-center lg:text-[18px]">
               Trusted by these companies
