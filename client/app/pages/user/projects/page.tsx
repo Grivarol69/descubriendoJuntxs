15372f42c3bd4c36fa26c202bfff91983141d2d8
bladex25 <colmenaresrm44@gmail.com>
2024-02-14 17:55:33 -0400

    remove bugs front

--- client/app/pages/user/projects/page.tsx
+++ client/app/pages/user/projects/page.tsx
@@ -5,42 +5,26 @@ import SearchBar from "@/components/searchbar/SearchBar"
 import style from './projects.module.css'
 
 
-export interface ProyectTypes {
-    nombre: string,
-    descripcion: string,
-    meta: string,
-    comentarios: string[]
-    imagen: string,
-    rating: string
+interface ProyectTypes {
+  id: number;
+  name: string;
+  description: string;
+  dateIn: string;
+  dateOut: string;
+  urlYoutube: string;
+  objective: string;
+  syllabus: string;
+  state: string;
+  categoryId: number;
+  type: string;
+  image: string;
+  commentary: [];
+
 }
 
 const UserProjectsPage = () => {
 
-    const proyecto = [{
-        nombre: 'Luis',
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloredsadas magnadasdasdsadsadsadsadsadsadas',
-        meta: 'Meta 200.000 - 300.000',
-        imagen: 'https://www.cajasietecontunegocio.com/images/recursos-humanos/rrhh-large.jpg',
-        comentarios: ['Hola', 'Adios'],
-        rating: '5'
-    },
-    {
-        nombre: 'Brigitte',
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloredsadas magnadasdasdsadsadsadsadsadsadas',
-        meta: 'Meta 200.000 - 300.000',
-        imagen: 'https://s1.abcstatics.com/media/bienestar/2021/02/04/risa-k60E--1248x698@abc.jpg',
-        comentarios: ['Hola', 'Adios'],
-        rating: '5'
-    },
-    {
-        nombre: 'Luci',
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloredsadas magnadasdasdsadsadsadsadsadsadas',
-        meta: 'Meta 200.000 - 300.000',
-        imagen: 'https://previews.123rf.com/images/leventegyori/leventegyori1506/leventegyori150600375/41665806-retrato-mayor-de-la-se%C3%B1ora.jpg',
-        comentarios: ['Hola', 'Adios'],
-        rating: '5'
-    }
-    ]
+    
 
     const [projects, setProjects] = useState<ProyectTypes[]>([...proyecto])
 
