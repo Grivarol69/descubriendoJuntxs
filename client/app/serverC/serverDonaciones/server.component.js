04b5f3bc49a8f49a70149a7c2d4b7e10c72694da
Luisfgt <luisfgonzalezt09@gmail.com>
2024-02-14 15:52:21 -0500

    add Admin Luciana Admin

---
+++ client/app/serverC/serverDonaciones/server.component.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import CardDonaciones from '../../../components/CardDonaciones/CardDonaciones';
+import DonacionesDetail from '../../../components/DonacionesDetail/DonacionesDetail';
+
+function formatDonation(donation) {
+    const formattedDonation = {
+      id: donation.id,
+      amount: donation.amount,
+      type: donation.type,
+      userId: donation.userId,
+      user: donation.user, 
+      date: donation.date,
+      project: donation.project, 
+    };
+  
+    return formattedDonation;
+  }
+
+  
+  function ServerComponent({ donations, onUserClick, onCloseDetail }) {
+    return (
+      <div>
+        <CardDonaciones
+          formattedDonations={donations.map(formatDonation)}
+          onCloseDetail={onCloseDetail}
+          onUserClick={onUserClick}
+          selectedUserId={null}
+        />
+        {donations.map((donation) => (
+          <DonacionesDetail
+            key={donation.id}
+            donation={donation}
+            project={donation.project}
+            onClose={onCloseDetail}
+          />
+        ))}
+      </div>
+    );
+  }
+
+export default ServerComponent;
